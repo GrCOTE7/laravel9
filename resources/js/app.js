7bcef11b5871cbbad21d676ced7130c7c03ace7a
Lionel COTE <grcote7@gmail.com>
2023-01-05 17:33:36 +0100

    Add JetStream

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import './bootstrap';
+
 import '../css/styles.css';
 import './gc7.js';
 
 import Alpine from 'alpinejs';
-
+import focus from '@alpinejs/focus';
 window.Alpine = Alpine;
+
+Alpine.plugin(focus);
 
 Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,14 @@ Route::get('/', function () {
 
 Route::get('t', [TestController::class, 'index'])->name('test');
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
 	return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-	Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-	Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-	Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+	Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
+	Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
+	Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require_once __DIR__ . '/auth.php';
